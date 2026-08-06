8b2feb33a4fc31440a5934b9c98afbfef2dfeb36
Darsh3008 <darshkanani326@gmail.com>
2026-08-06 13:13:50 +0530

    Update multer.js

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,14 +1,21 @@
-import multer from "multer"
+import fs from "fs";
+import path from "path";
+import multer from "multer";
+
+const uploadDir = path.join(process.cwd(), "public");
+
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public")
+    destination(req, file, cb) {
+        cb(null, uploadDir);
     },
-    filename: function (req, file, cb) {
-        const filename = Date.now() + "-" + file.originalname;
-        cb(null, filename)
-    }
-})
+    filename(req, file, cb) {
+        cb(null, `${Date.now()}-${file.originalname}`);
+    },
+});
 
 export const upload = multer({
     storage,
